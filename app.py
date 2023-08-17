0ae399110fb6e456a1703cb1a2a6bc5c9d9dba9e
Rodrigo Sakaguchi <rodrigosaka@outlook.com>
2023-08-16 22:28:53 -0300

    fix: corrige problemas de CSRF

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello Momy"
-
-if __name__ == '__main__':
-    app.run()
+    return "Laboratório Pipeline DevOps v1.0"
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Hello Momy")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World v1.0")

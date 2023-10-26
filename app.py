78aed8129146b1751721339494cba18bd5fc27a1
Rodrigo Sakaguchi <rodrigosaka@outlook.com>
2023-10-26 19:49:27 -0300

    feat: solution sprint fase 5

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps vFinal"
+    return "Laboratório Pipeline DevOps - Solution sprint fase 5"
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps vFinal")
+        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps - Solution sprint fase 5")
